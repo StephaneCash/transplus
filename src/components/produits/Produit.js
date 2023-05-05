ff6de4bf275e85e984c70b53ed07b3e03e1bb649
StéphaneCash <kikonistephane@gmail.com>
2023-05-05 13:11:56 +0200

    data

--- src/components/produits/Produit.js
+++ src/components/produits/Produit.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import "./Produit.css"
-import { FaArrowRight, FaChevronRight, FaEnvelope, FaEnvelopeOpenText, FaFacebook, FaInstagram, FaMapMarkerAlt, FaPhoneVolume, FaTwitter } from 'react-icons/fa'
+import { FiMail, FiFacebook, FiTwitter, FiInstagram, FiPhone } from 'react-icons/fi'
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { FaStreetView } from 'react-icons/fa';
 
 const Produit = () => {
 
@@ -29,9 +30,6 @@ const Produit = () => {
 
     return (
         <section id="produit">
-            <div className='head'>
-                <FaEnvelopeOpenText size={25} />
-            </div>
             <div className='container produit'>
                 <div class="form-group">
                     <input type="email" class="form-control" id="exampleInputEmail1"
@@ -48,49 +46,49 @@ const Produit = () => {
                 <div className='footer1'>
                     <ul>
                         <li>  Services</li>
-                        <li><FaChevronRight /> Commande de courses</li>
-                        <li><FaChevronRight /> Prêts de motos par contrat</li>
-                        <li><FaChevronRight /> Livraison de produits</li>
-                        <li><FaChevronRight /> Formations</li>
-                        <li> <FaChevronRight />Offre d'emploi</li>
+                        <li> Commande de courses</li>
+                        <li> Prêts de motos par contrat</li>
+                        <li> Livraison de produits</li>
+                        <li> Formations</li>
+                        <li> Offre d'emploi</li>
                     </ul>
 
                     <ul>
                         <li>Voulez-vous une course ?</li>
                         <li className='active'>
                             <Link to='/commander-une-course'>
-                                <FaArrowRight />   Commander votre course
+                                Commander votre course
                             </Link>
                         </li>
                         <li className='active'>
-                            <FaArrowRight />  Découvrir nos offres
+                            Découvrir nos offres
                         </li>
                     </ul>
 
                     <ul>
                         <li>Liens</li>
-                        <li><FaChevronRight />Accueil</li>
-                        <li><FaChevronRight />A propos</li>
-                        <li><FaChevronRight />Nos Services</li>
-                        <li><FaChevronRight />Offres</li>
-                        <li><FaChevronRight />Confiance</li>
+                        <li>Accueil</li>
+                        <li>A propos</li>
+                        <li>Nos Services</li>
+                        <li>Offres</li>
+                        <li>Confiance</li>
                     </ul>
 
                     <ul>
                         <li>Contact</li>
                         <li>
-                            <FaEnvelope /> <span>transplus@gmail.com</span>
+                            <FiMail /> <span>transplus@gmail.com</span>
                         </li>
                         <li>
-                            <FaPhoneVolume /> <span>+243 82 21 04 658</span>
+                            <FiPhone /> <span>+243 82 21 04 658</span>
                         </li>
                         <li>
-                            <FaMapMarkerAlt /> Kinshasa, Av. Matshumu 16, Debonhomme
+                            <FaStreetView /> Kinshasa, Debonhomme
                         </li>
                         <li className='reseauxSociaux'>
-                            <FaFacebook />
-                            <FaInstagram />
-                            <FaTwitter />
+                            <FiInstagram />
+                            <FiFacebook />
+                            <FiTwitter />
                         </li>
                     </ul>
                 </div>
--- src/components/securiser/Securiter.js
+++ src/components/securiser/Securiter.js
@@ -11,7 +11,9 @@ const Securiter = () => {
                 <div className='data'>
                     <div className='col-sm-6'>
                         <h4 className='title'>Nous proposons des offres d'emploi...</h4>
-                        <p>
+                        <p style={{
+                            textAlign:"justify"
+                        }}>
                             Processus incontournable de la vie de l'entreprise, le recrutement correspond à l'ensemble des actions mises en œuvre pour trouver le candidat qui correspond aux exigences de compétences (savoirs, savoir-faire et savoir-être) et de qualifications (diplômes et titres) requises pour un poste donné.
                             <br />
                             Consultez nos 28 offres d'emploi Entreprise en RD Congo en CDI ou CDD publiées sur Optioncarriere. Tous les postes à pourvoir en une seule recherche.
